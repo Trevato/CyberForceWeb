1cb08b1f29017388dfb1474347c32afcc26d2b7a
Trevor Dobbertin <trevordobbertin@gmail.com>
2019-11-07 14:38:58 -0600

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.conf import settings
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+def mail(request):
+    context = {
+        'ftpsrv': '10.0.%s.8' % settings.TEAM
+    }
+    return render(request, 'mail.html', context)
+
+
+def hmi(request):
+    context = {
+        'hmi': '10.0.%s.9' % settings.TEAM,
+        'ftpsrv': '10.0.%s.8' % settings.TEAM
+    }
+    return render(request, 'hmi.html', context)
+
+
+def notes(request):
+    context = {
+        'ftpsrv': '10.0.%s.8' % settings.TEAM,
+        'myip': '10.0.%s.6' % settings.TEAM
+    }
+    return render(request, 'notes.html', context)
+
+
+def files(request):
+    context = {
+        'ftpsrv': '10.0.%s.8' % settings.TEAM
+    }
+    return render(request, 'files.html', context)
+
+
+def index(request):
+    context = {
+        'ftpsrv': '10.0.%s.8' % settings.TEAM
+    }
+    return render(request, 'home.html', context)
