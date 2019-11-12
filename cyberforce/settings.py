e513b1454fde015f2c874e58d42b7d5b4fc920f9
Trevor Dobbertin <trevordobbertin@gmail.com>
2019-11-12 15:44:32 -0600

    Changed ldap uri back to working uri.

--- cyberforce/settings.py
+++ cyberforce/settings.py
@@ -22,7 +22,7 @@ AUTH_LDAP_USER_SEARCH = LDAPSearch(
     "cn=Users,dc=cybatiworks,dc=com", ldap.SCOPE_SUBTREE, "(uid=%(user)s)"
 )
 
-AUTH_LDAP_SERVER_URI = 'ldaps://ldap.cybatiworks.com/'
+AUTH_LDAP_SERVER_URI = ' ldap://ldap.cybatiworks.com'
 
 AUTHENTICATION_BACKENDS = [
                             'django_auth_ldap.backend.LDAPBackend',
