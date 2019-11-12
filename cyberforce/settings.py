77a398b7442b32d328ac4f6a98fda61fe8847bb2
Trevor Dobbertin <trevordobbertin@gmail.com>
2019-11-12 13:47:13 -0600

    Adjusted LDAP string again.

--- cyberforce/settings.py
+++ cyberforce/settings.py
@@ -16,7 +16,7 @@ from django_auth_ldap.config import LDAPSearch
 
 
 LDAP_STRING = "cn=Blueteam2,cn=Users,dc=cybatiworks,dc=com"
-AUTH_LDAP_BIND_DN = "cn=Blueteam2,cn=Users,dc=cybatiworks,dc=com"
+AUTH_LDAP_BIND_DN = "cn=Users,dc=cybatiworks,dc=com"
 AUTH_LDAP_BIND_PASSWORD = ""
 AUTH_LDAP_USER_SEARCH = LDAPSearch(
     "cn=Users,dc=cybatiworks,dc=com", ldap.SCOPE_SUBTREE, "(uid=%(user)s)"
