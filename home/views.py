0487cf7d3a05dd3eb425457a70f43a61bbb6c5ca
Trevor Dobbertin <trevordobbertin@gmail.com>
2019-11-12 18:21:26 -0600

    Fix logout redirect. Add link to login again.

--- home/views.py
+++ home/views.py
@@ -26,7 +26,7 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return render(request, 'login.html')
+    return render(request, 'logout.html')
 
 
 def support(request):
