1ab6c49263fb67da87f0d9dc25d5b5dcb3442761
Trevor Dobbertin <trevordobbertin@gmail.com>
2019-11-14 16:49:00 -0600

    Added referrer policy middleware.

--- cyberforce/settings.py
+++ cyberforce/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_referrer_policy.middleware.ReferrerPolicyMiddleware',
 ]
 
 MIDDLEWARE_CLASSES = (
