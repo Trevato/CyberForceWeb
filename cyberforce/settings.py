65fab09f57482007c3cc48f763a2b47ce9151809
Trevor Dobbertin <trevordobbertin@gmail.com>
2019-11-14 20:31:14 -0600

    Changed session cookie age to 1 minute for testing.

--- cyberforce/settings.py
+++ cyberforce/settings.py
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/login'
 HOME_REDIRECT_URL = '/home'
 
-SESSION_COOKIE_AGE = 30 * 60
+SESSION_COOKIE_AGE = 60
 
 from netifaces import AF_INET
 import netifaces as ni
