c31cf42d3cf856f4bebc518e8f3d8e4b80bd05b1
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-02-04 17:47:25 -0600

    Revert.

--- cyberforce/settings.py
+++ cyberforce/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-LOGIN_REDIRECT_URL = '/hmi'
+LOGIN_REDIRECT_URL = '/login'
 LOGIN_URL = '/login'
 STATIC_URL = '/static/'
 
